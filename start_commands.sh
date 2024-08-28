e6369a506b4136b8badc0943ca1b9f93c62a5e98
jackis147258 <masdiothanwordba@mail.com>
2024-08-28 20:28:57 +0800

    ooo

--- start_commands.sh
+++ start_commands.sh
@@ -1,5 +1,5 @@
 #!/bin/bash      
-nohup daphne -b 0.0.0.0 -p 9003 moon39.asgi:application > /www/wwwroot/nodekuangji/nohubdaphne.log 2>&1 &
-nohup celery -A moon39 worker --loglevel=info > /www/wwwroot/nodekuangji/celerymoon39.log 2>&1 &
-nohup celery -A moon39 beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler > /www/wwwroot/nodekuangji/celerymoon39beat.log 2>&1 &
+nohup daphne -b 0.0.0.0 -p 9003 moon39.asgi:application > /www/wwwroot/nodekuangjiGB/nohubdaphne.log 2>&1 &
+nohup celery -A moon39 worker --loglevel=info > /www/wwwroot/nodekuangjiGB/celerymoon39.log 2>&1 &
+nohup celery -A moon39 beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler > /www/wwwroot/nodekuangjiGB/celerymoon39beat.log 2>&1 &
 
